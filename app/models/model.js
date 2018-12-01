82c8b89e2ea34eb7996ca3f1730c0edade487e20
Irvin Lee Nelson <irvinlnelson@Irvins-MacBook-Pro.local>
2018-12-01 10:52:13 -0800

    irvins push

---
+++ app/models/model.js
@@ -0,0 +1,39 @@
+// Dependencies
+// =============================================================
+
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "User" model that matches up with DB
+var User = sequelize.define("users", {
+    // the username gets saved as a string
+    username: Sequelize.STRING,
+    // email gets saved as a string
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+            isEmail: true,
+            notNull: true,
+            len: [1, 35]
+        }
+    },
+    // password gets saved as a string
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 18],
+            is: ["^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})", "i"]
+        }
+    }
+});
+
+
+
+// Syncs with DB
+User.sync();
+
+// Makes the User Model available for other files (will also create a table)
+module.exports = User;
