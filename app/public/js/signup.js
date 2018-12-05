5c5c484d3a57c94105995ee3870210ad976e8191
Norton Nice <nortonnice@gmail.com>
2018-12-05 12:22:56 -0800

    index.html changes working with Edgar last Night

--- app/public/js/signup.js
+++ app/public/js/signup.js
@@ -1,21 +1,25 @@
-function signUp () {
-    
-    event.preventDefault();
-    var newUser = {
-        username: $("#username").val().trim(),
-        email: $("#email").val().trim(),
-        password: $("#psw").val().trim(),
-        stock1: "",
-        stock2: "",
-        stock3: ""
-    };
-    console.log(newUser);
+$(document).ready(function () {
 
-    //sends request to create a new user in the database
-    $.post("/api/new", newUser)
-        .then(function(data){
-            console.log(data);
-            $("<caption>").html(data.username + "'s Watchlist")
-        })
+    function signUp(event) {
+        event.preventDefault();
+        var newUser = {
+            username: $("#username").val().trim(),
+            email: $("#email").val().trim(),
+            password: $("#psw").val().trim(),
+            stock1: "",
+            stock2: "",
+            stock3: ""
+        };
 
-}
+        console.log(newUser);
+
+        //sends request to create a new user in the database
+        $.post("./api/new", newUser)
+            .then(function (data) {
+                console.log(data);
+                $("<caption>").html(data.username + "'s Watchlist")
+            }).catch(err => console.log(err))
+    }
+
+    $("#signUpBtn").on("click", signUp)
+})
